6b89f0440a5895fffd61d720909eb942520ac79c
Makeris <makeris616@gmail.com>
2021-03-31 23:55:57 +0300

    'JSAdvanced_10_Cenzor'

---
+++ Cenzor/main.js
@@ -0,0 +1,42 @@
+// console.log(-4%2);
+
+// let a = 2;
+
+// a != 0 ? a%2 > 0 ? console.log('not parne') : console.log('parne') : console.log(a);
+
+// isPrime(a); 
+
+// function isPrime(num) {
+
+//     // let number = 0;
+
+//     if(num != 0) {
+
+//         if(num % 2 == 0) {
+//             console.log('Parne');    
+//         } else if (num % 2 > 0 ) {
+//             console.log('Not parne');
+//         } else if (num % 2 < 0 ) {
+//             console.log('Not parne');
+//         }
+//     } else {
+//         console.log('Zero');
+//     }
+
+
+
+// }
+
+// console.log(Math.max(-5,2));
+
+
+// function a(param) {
+//     console.log(Math.max(...arguments));
+// }
+// a(11,25,3,4,5,6);
+
+
+// console.log(Math.sqrt(4));
+
+// let f1 = document.forms['f1'];
+// console.log(typeof f1);
